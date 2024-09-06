cd8608d9aefee9966c201d65c5d6d631eb6f7c09
h3nr1d3v <akame8416@gmail.com>
2024-09-05 19:31:09 -0500

    Modificando music.py

--- cogs/music.py
+++ cogs/music.py
@@ -39,10 +39,11 @@ class YTDLSource(discord.PCMVolumeTransformer):
     async def from_url(cls, url, *, loop=None, stream=False):
         loop = loop or asyncio.get_event_loop()
         try:
-            data = await loop.run_in_executor(None, lambda: ytdl.extract_info(url, download=not stream))
+            data = await loop.run_in_executor(None, lambda: youtube_dl.extract_info(url, download=not stream))
             if 'entries' in data:
                 data = data['entries'][0]
-            filename = data['url'] if stream else ytdl.prepare_filename(data)
+            filename = data['url'] if stream else youtube_dl.prepare_filename(
+                data)
             return cls(discord.FFmpegPCMAudio(filename, **ffmpeg_options), data=data)
         except Exception as e:
             print(f"Error in from_url: {e}")
