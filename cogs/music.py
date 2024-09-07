046e59e6c20c5788dacba6d891897e74c554efd1
h3nr1d3v <akame8416@gmail.com>
2024-09-06 21:14:28 -0500

    Modificando music.py

--- cogs/music.py
+++ cogs/music.py
@@ -31,7 +31,7 @@ yt_dl_options = {
 
 ffmpeg_options = {
     'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5',
-    'options': '-vn -filter:a "volume=0.25"'
+    'options': '-vn -filter:a "volume=1"'
 }
 
 ytdl = yt_dlp.YoutubeDL(yt_dl_options)
@@ -50,15 +50,30 @@ class MusicCog(commands.Cog):
             'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
         ]
 
-    async def send_banner(self, ctx, track):
+    async def get_random_image(self):
         async with ClientSession(headers={'User-Agent': random.choice(self.user_agents)}) as session:
-            async with session.get(f'https://nekos.best/api/v2/music') as resp:
+            async with session.get('https://nekos.best/api/v2/music') as resp:
                 data = await resp.json()
-                embed = discord.Embed(
-                    title="Now Playing", description=f"{track['title']}")
                 if 'results' in data and len(data['results']) > 0:
-                    embed.set_image(url=data['results'][0]['url'])
-                await ctx.send(embed=embed)
+                    return data['results'][0].get('url', 'https://example.com/default_image.png')
+                else:
+                    # Manejar caso donde 'results' no está presente o está vacío
+                    return 'https://example.com/default_image.png'  # URL de imagen por defecto
+
+    async def create_embed(self, title, description, color=discord.Color.blue()):
+        embed = discord.Embed(
+            title=title, description=description, color=color)
+        try:
+            image_url = await self.get_random_image()
+            embed.set_image(url=image_url)
+        except Exception as e:
+            # Manejar el caso donde no se puede obtener la imagen
+            print(f"Error getting image: {e}")
+        return embed
+
+    async def send_embed(self, ctx, title, description, color=discord.Color.blue()):
+        embed = await self.create_embed(title, description, color)
+        await ctx.send(embed=embed)
 
     async def play_next(self, ctx):
         if ctx.guild.id in self.queues and self.queues[ctx.guild.id]:
@@ -68,7 +83,7 @@ class MusicCog(commands.Cog):
             next_song = self.queues[ctx.guild.id].pop(0)
             await self.play_song(ctx, next_song)
         else:
-            await ctx.send("Queue is empty. Playback finished.")
+            await self.send_embed(ctx, "Queue Empty", "Playback finished.")
 
     async def play_song(self, ctx, song):
         try:
@@ -82,11 +97,11 @@ class MusicCog(commands.Cog):
                 self.voice_clients[ctx.guild.id].play(
                     player, after=lambda e: asyncio.run_coroutine_threadsafe(self.play_next(ctx), self.bot.loop))
                 self.current_songs[ctx.guild.id] = song
-                await self.send_banner(ctx, song)
+                await self.send_embed(ctx, "Now Playing", song['title'])
             else:
-                await ctx.send("Not connected to a voice channel.")
+                await self.send_embed(ctx, "Error", "Not connected to a voice channel.")
         except Exception as e:
-            await ctx.send(f"An error occurred: {str(e)}")
+            await self.send_embed(ctx, "Error", f"An error occurred: {str(e)}", discord.Color.red())
 
     async def get_youtube_results(self, query):
         search_query = urllib.parse.urlencode({'search_query': query})
@@ -110,7 +125,7 @@ class MusicCog(commands.Cog):
     async def play(self, ctx, *, query):
         await asyncio.sleep(random.uniform(1, 3))  # Add a random delay
         if ctx.author.voice is None:
-            await ctx.send("You need to be in a voice channel to use this command.")
+            await self.send_embed(ctx, "Error", "You need to be in a voice channel to use this command.", discord.Color.red())
             return
 
         if ctx.guild.id not in self.voice_clients:
@@ -119,18 +134,21 @@ class MusicCog(commands.Cog):
 
         search_results = await self.get_youtube_results(query)
         if not search_results:
-            await ctx.send("No results found.")
+            await self.send_embed(ctx, "No Results", "No results found.", discord.Color.red())
             return
 
         # Get info for each video
         videos = []
+        description = ""
         for i, video_id in enumerate(search_results, 1):
             video_info = await self.get_video_info(video_id)
             videos.append(video_info)
-            await ctx.send(f"{i}. {video_info['title']} - {video_info['uploader']} ({video_info['duration']} seconds)")
+            description += f"{i}. {video_info['title']} - {video_info['uploader']} ({video_info['duration']} seconds)\n"
 
-        # Ask user to choose a video
-        await ctx.send("Please choose a video by entering a number from 1 to 5.")
+        embed = await self.create_embed("Search Results", description)
+        embed.set_footer(
+            text="Please choose a video by entering a number from 1 to 5.")
+        await ctx.send(embed=embed)
 
         def check(m):
             return m.author == ctx.author and m.channel == ctx.channel and m.content.isdigit() and 1 <= int(m.content) <= 5
@@ -138,7 +156,7 @@ class MusicCog(commands.Cog):
         try:
             msg = await self.bot.wait_for('message', check=check, timeout=30.0)
         except asyncio.TimeoutError:
-            await ctx.send("You didn't choose a video in time.")
+            await self.send_embed(ctx, "Timeout", "You didn't choose a video in time.", discord.Color.red())
             return
 
         chosen_video = videos[int(msg.content) - 1]
@@ -151,49 +169,57 @@ class MusicCog(commands.Cog):
         if not self.voice_clients[ctx.guild.id].is_playing():
             await self.play_song(ctx, chosen_video)
         else:
-            await ctx.send(f"Added to queue: {chosen_video['title']}")
+            await self.send_embed(ctx, "Added to Queue", f"Added to queue: {chosen_video['title']}")
 
     @commands.command(name='pause')
     async def pause(self, ctx):
         if ctx.guild.id in self.voice_clients and self.voice_clients[ctx.guild.id].is_playing():
             self.voice_clients[ctx.guild.id].pause()
-            await ctx.send("Music paused.")
+            await self.send_embed(ctx, "Paused", "Music paused.")
+        else:
+            await self.send_embed(ctx, "Error", "No music is playing.", discord.Color.red())
 
     @commands.command(name='resume')
     async def resume(self, ctx):
         if ctx.guild.id in self.voice_clients and self.voice_clients[ctx.guild.id].is_paused():
             self.voice_clients[ctx.guild.id].resume()
-            await ctx.send("Resuming music.")
+            await self.send_embed(ctx, "Resumed", "Resuming music.")
+        else:
+            await self.send_embed(ctx, "Error", "No music is paused.", discord.Color.red())
 
     @commands.command(name='skip')
     async def skip(self, ctx):
         if ctx.guild.id in self.voice_clients and self.voice_clients[ctx.guild.id].is_playing():
             self.voice_clients[ctx.guild.id].stop()
-            await ctx.send("Skipped to next track.")
+            await self.send_embed(ctx, "Skipped", "Skipped to next track.")
+        else:
+            await self.send_embed(ctx, "Error", "No music is playing.", discord.Color.red())
 
     @commands.command(name='queue')
     async def queue(self, ctx):
         if ctx.guild.id in self.queues and self.queues[ctx.guild.id]:
             queue_list = "\n".join(
                 [f"{i+1}. {track['title']}" for i, track in enumerate(self.queues[ctx.guild.id])])
-            await ctx.send(f"Current Queue:\n{queue_list}")
+            await self.send_embed(ctx, "Current Queue", queue_list)
         else:
-            await ctx.send("Queue is empty.")
+            await self.send_embed(ctx, "Queue Empty", "Queue is empty.")
 
     @commands.command(name='clear_queue')
     async def clear_queue(self, ctx):
         if ctx.guild.id in self.queues:
             self.queues[ctx.guild.id].clear()
-            await ctx.send("Queue cleared!")
+            await self.send_embed(ctx, "Queue Cleared", "Queue cleared!")
         else:
-            await ctx.send("There is no queue to clear")
+            await self.send_embed(ctx, "Error", "There is no queue to clear", discord.Color.red())
 
     @commands.command(name='leave')
     async def leave(self, ctx):
         if ctx.guild.id in self.voice_clients:
             await self.voice_clients[ctx.guild.id].disconnect()
             del self.voice_clients[ctx.guild.id]
-            await ctx.send("Disconnected from voice channel.")
+            await self.send_embed(ctx, "Disconnected", "Disconnected from voice channel.")
+        else:
+            await self.send_embed(ctx, "Error", "Not connected to a voice channel.", discord.Color.red())
 
     @commands.command(name='join')
     async def join(self, ctx):
@@ -202,24 +228,25 @@ class MusicCog(commands.Cog):
             if ctx.guild.id not in self.voice_clients:
                 voice_client = await channel.connect()
                 self.voice_clients[ctx.guild.id] = voice_client
-                await ctx.send(f"Joined {channel.name}")
+                await self.send_embed(ctx, "Joined", f"Joined {channel.name}")
             else:
-                await ctx.send("Already connected to a voice channel.")
+                await self.send_embed(ctx, "Already Connected", "Already connected to a voice channel.")
         else:
-            await ctx.send("You need to be in a voice channel to use this command.")
+            await self.send_embed(ctx, "Error", "You need to be in a voice channel to use this command.", discord.Color.red())
 
     @commands.command(name='loop')
     async def loop(self, ctx):
         self.loop[ctx.guild.id] = not self.loop.get(ctx.guild.id, False)
-        await ctx.send(f"Loop {'enabled' if self.loop[ctx.guild.id] else 'disabled'}.")
+        status = "enabled" if self.loop[ctx.guild.id] else "disabled"
+        await self.send_embed(ctx, "Loop", f"Loop {status}.")
 
     @commands.command(name='reboot')
     async def reboot(self, ctx):
         if ctx.guild.id in self.current_songs:
             await self.play_song(ctx, self.current_songs[ctx.guild.id])
-            await ctx.send("Rebooting current song.")
+            await self.send_embed(ctx, "Rebooted", "Rebooting current song.")
         else:
-            await ctx.send("No song is currently playing.")
+            await self.send_embed(ctx, "Error", "No song is currently playing.", discord.Color.red())
 
 
 async def setup(bot):
